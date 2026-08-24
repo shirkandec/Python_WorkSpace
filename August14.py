4160642263269f2c91bf99f4afd7a1f11f846e09
Chaitanya Shirkande <chaitanyashirkande@Chaitanyas-MacBook-Air.local>
2026-08-24 18:39:44 +0530

    Initial commit

---
+++ August14.py
@@ -0,0 +1,191 @@
+
+# Sname="Chaitanya Shirkande "
+# Age=20
+# Percentage=90.5
+# is_placed=True
+
+# print(Sname)
+# print(Age)
+# print(Percentage)
+# print(is_placed)
+
+# print(type(Sname))
+# print(type(Age))
+# print(type(Percentage))
+# print(type(is_placed))
+
+# a=int(input("Enter the age of student: "))
+# print(a)
+# print(type(a))
+# a+=5
+# print(a)
+
+# a=(25,40,18,67,32)
+# print(a)
+# print(max(a))
+# print(min(a))
+# print(sum(a))
+# print(sum(a)/len(a)) 
+# print(type(a))
+
+
+# a=3.14159
+# print(a)
+# print(type(a))
+# complex_a=3+5j
+# print(complex_a)
+# print(type(complex_a))
+
+# a="Chaitanya Vijaykumar Shirkande"
+# print(a)
+# print(type(a))
+# print(abs(-125))
+
+# a=78.4567
+# print(a)
+# print(type(a))
+# print(round(a, 2))
+
+
+# a=int(input("Enter a number:"))
+# print(a**2)
+# print(a**3)
+# print(pow(a,2))
+# print(pow(a,3))
+
+
+# a=(int(input("enter first number:")))
+# b=int(input("enter second number:"))
+# c=a+b
+# print("addition of two numbers is:",c)
+# d=a-b
+# print("Sub of two numbers is:",d)
+# e=a*b
+# print("multiplication of two numbers is:",e)
+# f=a/b
+# print("division of two numbers is:",f)
+# g=a%b
+# print("Remainder of this division is : ",g)
+# h=a//b
+# print("Floor division of two number is:",h)
+# i=a**b
+# print("Power of two number is:",i)
+
+# a=int(input("Enter the first number:"))
+# b= int(input("Enter the second number:"))
+# print(a<b)
+# print(a>b)
+# print(a==b)
+# print(a!=b)
+# print(a<=b)
+# print(a>=b)
+
+##Start with x = 100.
+# Apply += 20, -= 10, *= 2 and /= 5 one by one and print x after every operation.
+
+# x=100
+# x += 20
+# print(x)
+# x -= 10
+# print(x)
+# x *= 2
+# print(x)
+# x /= 5  
+# print(x)
+
+# marks=int(input("Enter the marks of student:"))
+# if marks>=60:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# num=int(input("ENter the number "))
+# if num%2==0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# s1=( "KiranAcademy")
+# print(s1)
+# print(s1[0])
+# print(s1[-1])
+# print(s1[4])
+# print(len(s1))
+
+
+# s2=("Programming ")
+# print(s2[0:5])
+# print(s2[::-1])
+# print(s2[-5:-1])
+
+# s3=(" python full stack ")
+# print(s3. strip())
+# print(s3.upper())
+# print(s3.lower())
+# print(s3.title())
+# print(s3.replace("full stack","Django"))
+
+# s4=("Java Python Testing Data")
+# print("Python" in s4)
+# print (s4.find("Testing"))
+
+# s5=("banana")
+# print(s5.count("a"))
+# print(s5.count("n"))
+
+# s6=("Kiran Academy Pune")
+# a=(s6.startswith("Kiran"))
+# b=(s6.endswith("Pune"))
+# print(a and b)
+
+# s7=("Java,Python,React,SQL")
+# print(s7.split(","))
+
+
+
+
+# s8=("Java,Python,React,SQL")
+# a=(s8.split(","))
+# print("-".join(a))
+
+
+# word = input("Enter a word: ")
+# if word.isupper():
+#     print("Uppercase")
+# elif word.islower():
+#     print("Lowercase")
+# else:   
+#     print("Neither")
+
+
+
+
+
+
+
+# text = "I am learning Java"
+# new_text = text.replace("Java", "Python")
+# print(new_text)
+
+
+
+
+# name = input("Enter student name: ")
+# course = input("Enter course: ")
+# marks = float(input("Enter marks: "))
+# if marks >= 60:
+#     eligibility = "Eligible"
+# else:
+#     eligibility = "Not Eligible"
+# print(f"Student: {name}, Course: {course}, Marks: {marks}, Status: {eligibility}")
+
+
+
+# pname = input("Enter product name: ")
+# quantity = int(input("Enter quantity: "))
+# price = float(input("Enter price per unit: "))
+# total = quantity * price
+# print(f"Product: {pname}")
+# print(f"Quantity: {quantity}")
+# print(f"Price per Unit: {price}")
+# print(f"Total Bill: {total}")
---
+++ demo.py
@@ -0,0 +1,5 @@
+year=int(input("Enter year: "))
+if (year % 4) == 0:
+    print("{0} is a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(year))
